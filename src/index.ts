74299a5147069c4858c5b5d45f82db7d95680e0c
xeusteerapat <xeus085@gmail.com>
2020-10-31 20:42:43 +0700

    [update] products type

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,49 @@
-import { queryType, makeSchema } from '@nexus/schema';
+import { queryType, mutationType, objectType, makeSchema } from '@nexus/schema';
 import { ApolloServer } from 'apollo-server';
+
+// Products
+const Product = objectType({
+  name: 'Product',
+  definition(t) {
+    t.id('id');
+    t.string('name');
+  },
+});
+
+// Reviews (one-to-many)
+
+// Categories (many-to-many)
 
 const Query = queryType({
   definition(t) {
-    t.string('hello', {
-      resolve: () => 'hello Nexus',
+    t.list.field('products', {
+      type: 'Product',
+      resolve: () => {
+        return [
+          {
+            id: '1',
+            name: 'Camera',
+          },
+          {
+            id: '2',
+            name: 'Mobile Phone',
+          },
+          {
+            id: '3',
+            name: 'Tablet',
+          },
+        ];
+      },
     });
   },
 });
 
 const schema = makeSchema({
-  types: [Query],
+  types: [Query, Product],
+  outputs: {
+    schema: __dirname + '/generated/schema.graphql',
+    typegen: __dirname + '/generated/typings.ts',
+  },
 });
 
 const server = new ApolloServer({
@@ -18,5 +51,5 @@ const server = new ApolloServer({
 });
 
 server.listen().then(({ port }) => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port} 🚀`);
 });
